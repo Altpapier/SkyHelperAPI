359b7f69a8b07513d7817eeb48f0d05bd883b6bc
Altpapier <altpapieryt@gmail.com>
2021-10-10 13:22:03 +0200

    automatic updates

--- middleware/checkforupdate.js
+++ middleware/checkforupdate.js
@@ -1,9 +1,10 @@
-const { red, white } = require('chalk')
+const { red, green } = require('chalk')
 const { exec } = require('child_process');
 
 module.exports = function checkforupdate() {
 
-    exec('git status', (err, stdout, stderr) => {
-        //Coming Soon!!
+    exec('git pull', (err, stdout, stderr) => {
+        if (stdout.includes('Already up to date.')) console.log(green('The API is up to date! No updates made.'))
+        else if (stdout.includes('Updating')) console.log(red('Updated the API. Please restart the API to be up to date.'))
     });
 }
