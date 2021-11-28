1693b3d39d0f647f582595cac281b469efc903d6
Altpapier <altpapieryt@gmail.com>
2021-11-28 15:54:04 +0100

    FIX

--- stats/weight.js
+++ stats/weight.js
@@ -14,19 +14,19 @@ module.exports = (profile) => {
             taming: calculateWeight('taming', (calcSkill('taming', profile?.experience_skill_taming || 0)).level, profile?.experience_skill_taming || 0)
         },
         slayer: {
-            revenant: calculateWeight('revenant', null, profile.slayer_bosses?.zombie.xp || 0),
-            tarantula: calculateWeight('tarantula', null, profile.slayer_bosses?.spider.xp || 0),
-            sven: calculateWeight('sven', null, profile.slayer_bosses?.wolf.xp || 0),
+            revenant: calculateWeight('revenant', null, profile.slayer_bosses?.zombie?.xp || 0),
+            tarantula: calculateWeight('tarantula', null, profile.slayer_bosses?.spider?.xp || 0),
+            sven: calculateWeight('sven', null, profile.slayer_bosses?.wolf?.xp || 0),
             enderman: calculateWeight('enderman', null, profile.slayer_bosses?.enderman?.xp || 0)
         },
         dungeons: {
-            catacombs: calculateWeight('catacombs', (calcSkill('dungeoneering', profile.dungeons?.dungeon_types.catacombs.experience || 0)).level, profile.dungeons?.dungeon_types.catacombs.experience),
+            catacombs: calculateWeight('catacombs', (calcSkill('dungeoneering', profile.dungeons?.dungeon_types?.catacombs?.experience || 0)).level, profile.dungeons?.dungeon_types?.catacombs?.experience  || 0),
             classes: {
-                healer: calculateWeight('healer', (calcSkill('dungeoneering', profile.dungeons?.player_classes.healer.experience || 0)).level, profile.dungeons?.player_classes.healer.experience),
-                mage: calculateWeight('mage', (calcSkill('dungeoneering', profile.dungeons?.player_classes.mage.experience || 0)).level, profile.dungeons?.player_classes.mage.experience),
-                berserk: calculateWeight('berserk', (calcSkill('dungeoneering', profile.dungeons?.player_classes.berserk.experience || 0)).level, profile.dungeons?.player_classes.berserk.experience),
-                archer: calculateWeight('archer', (calcSkill('dungeoneering', profile.dungeons?.player_classes.archer.experience || 0)).level, profile.dungeons?.player_classes.archer.experience),
-                tank: calculateWeight('tank', (calcSkill('dungeoneering', profile.dungeons?.player_classes.tank.experience || 0)).level, profile.dungeons?.player_classes.tank.experience),
+                healer: calculateWeight('healer', (calcSkill('dungeoneering', profile.dungeons?.player_classes?.healer?.experience || 0)).level, profile.dungeons?.player_classes?.healer?.experience || 0),
+                mage: calculateWeight('mage', (calcSkill('dungeoneering', profile.dungeons?.player_classes?.mage?.experience || 0)).level, profile.dungeons?.player_classes?.mage?.experience || 0),
+                berserk: calculateWeight('berserk', (calcSkill('dungeoneering', profile.dungeons?.player_classes?.berserk?.experience || 0)).level, profile.dungeons?.player_classes?.berserk?.experience || 0),
+                archer: calculateWeight('archer', (calcSkill('dungeoneering', profile.dungeons?.player_classes?.archer?.experience || 0)).level, profile.dungeons?.player_classes?.archer?.experience || 0),
+                tank: calculateWeight('tank', (calcSkill('dungeoneering', profile.dungeons?.player_classes?.tank?.experience || 0)).level, profile.dungeons?.player_classes?.tank?.experience || 0),
             }
         }
     }
