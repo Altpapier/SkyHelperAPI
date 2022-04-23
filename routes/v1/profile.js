4873afa7c2df5e89d88e2009dbdea1521612409c
Altpapier <robin2leo@gmail.com>
2022-04-23 09:52:03 +0200

    dont use official mojang api for profile

--- routes/v1/profile.js
+++ routes/v1/profile.js
@@ -1,15 +1,15 @@
 //CREDIT: https://github.com/Senither/hypixel-skyblock-facade (Modified)
 const { isUuid } = require('../../utils/uuid');
-const { makeRequest, wrap } = require('../../utils/request')
-const { parseHypixel, parseProfile } = require('../../utils/hypixel')
+const { makeRequest, wrap } = require('../../utils/request');
+const { parseHypixel, parseProfile } = require('../../utils/hypixel');
 
 module.exports = wrap(async function (req, res) {
     const profileid = req.params.profileid;
     let uuid = req.params.uuid;
     if (!isUuid(uuid)) {
-        const mojang_response = await makeRequest(res, `https://api.mojang.com/users/profiles/minecraft/${uuid}`)
-        if (mojang_response.data.id) {
-            uuid = mojang_response.data.id
+        const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`);
+        if (mojang_response?.data?.uuid) {
+            uuid = mojang_response.data.uuid.replace(/-/g, '');
         }
     }
 
@@ -19,6 +19,5 @@ module.exports = wrap(async function (req, res) {
     const profileRes = await makeRequest(res, `https://api.hypixel.net/skyblock/profiles?key=${process.env.HYPIXEL_API_KEY}&uuid=${uuid}`);
     const profile = await parseProfile(player, profileRes, uuid, profileid, res);
 
-
-    return res.status(200).json({status: 200, data: profile});
-})
+    return res.status(200).json({ status: 200, data: profile });
+});
--- routes/v1/profiles.js
+++ routes/v1/profiles.js
@@ -1,14 +1,14 @@
 //CREDIT: https://github.com/Senither/hypixel-skyblock-facade (Modified)
 const { isUuid } = require('../../utils/uuid');
-const { makeRequest, wrap } = require('../../utils/request')
-const { parseHypixel, parseProfiles } = require('../../utils/hypixel')
+const { makeRequest, wrap } = require('../../utils/request');
+const { parseHypixel, parseProfiles } = require('../../utils/hypixel');
 
 module.exports = wrap(async function (req, res) {
     let uuid = req.params.uuid;
     if (!isUuid(uuid)) {
-        const mojang_response = await makeRequest(res, `https://api.mojang.com/users/profiles/minecraft/${uuid}`)
-        if (mojang_response.data.id) {
-            uuid = mojang_response.data.id
+        const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`);
+        if (mojang_response?.data?.uuid) {
+            uuid = mojang_response.data.uuid.replace(/-/g, '');
         }
     }
 
@@ -18,6 +18,5 @@ module.exports = wrap(async function (req, res) {
     const profileRes = await makeRequest(res, `https://api.hypixel.net/skyblock/profiles?key=${process.env.HYPIXEL_API_KEY}&uuid=${uuid}`);
     const profile = await parseProfiles(player, profileRes, uuid, res);
 
-
-    return res.status(200).json({status: 200, data: profile});
-})
+    return res.status(200).json({ status: 200, data: profile });
+});
