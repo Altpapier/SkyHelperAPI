5ee50ab9439de9e55af5ca7849c1198782079eff
Altpapier <robin2leo@gmail.com>
2022-06-12 15:31:33 +0200

    fix(talismans): fixed breaking bc of special

--- stats/talismans.js
+++ stats/talismans.js
@@ -1,6 +1,6 @@
-const { decodeData } = require('../utils/nbt')
-const { capitalize } = require('../constants/functions')
-const { talismans: allTalismans } = require('../constants/talismans')
+const { decodeData } = require('../utils/nbt');
+const { capitalize } = require('../constants/functions');
+const { talismans: allTalismans } = require('../constants/talismans');
 
 module.exports = async (profile) => {
     if (profile.talisman_bag?.data) {
@@ -10,31 +10,32 @@ module.exports = async (profile) => {
             rare: [],
             epic: [],
             legendary: [],
-            mythic: []
-        }
-        const talisman_bag = (await decodeData(Buffer.from(profile.talisman_bag.data, 'base64'))).i
+            mythic: [],
+            special: [],
+        };
+        const talisman_bag = (await decodeData(Buffer.from(profile.talisman_bag.data, 'base64'))).i;
 
         for (const talisman of talisman_bag) {
             if (talisman.tag?.display.Name && talisman.tag?.ExtraAttributes) {
                 //If there is a reforge, remove the reforge from the item name
-                let name = talisman.tag?.display.Name.replace(/\u00A7[0-9A-FK-OR]/ig, '') || null
-                const reforge = capitalize(talisman.tag?.ExtraAttributes.modifier || null)
-                if (reforge) name = name.substring(name.indexOf(' ') + 1)
-                const isRecombed = talisman.tag?.ExtraAttributes.rarity_upgrades > 0 ? true : false || false
+                let name = talisman.tag?.display.Name.replace(/\u00A7[0-9A-FK-OR]/gi, '') || null;
+                const reforge = capitalize(talisman.tag?.ExtraAttributes.modifier || null);
+                if (reforge) name = name.substring(name.indexOf(' ') + 1);
+                const isRecombed = talisman.tag?.ExtraAttributes.rarity_upgrades > 0 ? true : false || false;
 
                 const new_talisman = {
                     name: allTalismans[talisman.tag?.ExtraAttributes.id]?.name || name,
                     id: talisman.tag?.ExtraAttributes.id || null,
                     reforge: reforge,
                     rarity: getRarity(talisman.tag?.display.Lore).toUpperCase(),
-                    recombobulated: isRecombed
-                }
-                if (talismans[getRarity(talisman.tag?.display.Lore)]) talismans[getRarity(talisman.tag?.display.Lore)].push(new_talisman)
-                else talismans[getRarity(talisman.tag?.display.Lore)] = new_talisman
+                    recombobulated: isRecombed,
+                };
+                if (talismans[getRarity(talisman.tag?.display.Lore)]) talismans[getRarity(talisman.tag?.display.Lore)].push(new_talisman);
+                else talismans[getRarity(talisman.tag?.display.Lore)] = new_talisman;
             }
         }
 
-        return talismans
+        return talismans;
     } else {
         return {
             common: [],
@@ -42,15 +43,15 @@ module.exports = async (profile) => {
             rare: [],
             epic: [],
             legendary: [],
-            mythic: []
-        }
+            mythic: [],
+        };
     }
-}
+};
 
 function getRarity(lore) {
-    let last_index = lore[lore.length -1]
-    last_index = last_index.replace(/\u00A7[0-9A-FK-OR]/ig, '').toLowerCase()
-    if (last_index.startsWith('a ')) last_index = last_index.substring(2)
-    last_index = last_index.substring(0, last_index.indexOf(' '))
-    return last_index
+    let last_index = lore[lore.length - 1];
+    last_index = last_index.replace(/\u00A7[0-9A-FK-OR]/gi, '').toLowerCase();
+    if (last_index.startsWith('a ')) last_index = last_index.substring(2);
+    last_index = last_index.substring(0, last_index.indexOf(' '));
+    return last_index;
 }
