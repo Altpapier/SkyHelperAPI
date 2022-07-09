787b01e4b6a292f30e3de1ace1c832ec83cfaca0
Altpapier <robin2leo@gmail.com>
2022-07-09 14:50:53 +0200

    fix social v2

--- constants/skills.js
+++ constants/skills.js
@@ -1,10 +1,11 @@
 //CREDIT: https://github.com/SkyCrypt/SkyCryptWebsite (Modified)
-const xp_tables = require('./xp_tables')
+const xp_tables = require('./xp_tables');
 
 module.exports = function calcSkill(skill, experience) {
-    table = 'normal'
-    if (skill === 'runecrafting' || skill === 'social') table = 'runecrafting'
-    if (skill === 'dungeoneering') table = 'catacombs'
+    table = 'normal';
+    if (skill === 'runecrafting') table = 'runecrafting';
+    if (skill === 'social') table = 'social';
+    if (skill === 'dungeoneering') table = 'catacombs';
 
     if (experience <= 0) {
         return {
@@ -13,31 +14,31 @@ module.exports = function calcSkill(skill, experience) {
             xpCurrent: 0,
             xpForNext: xp_tables[table][0],
             progress: 0,
-        }
+        };
     }
 
-    let xp = 0
-    let level = 0
-    let xpForNext = 0
-    let progress = 0
-    let maxLevel = 0
+    let xp = 0;
+    let level = 0;
+    let xpForNext = 0;
+    let progress = 0;
+    let maxLevel = 0;
 
-    if (xp_tables.max_levels[skill]) maxLevel = xp_tables.max_levels[skill] 
+    if (xp_tables.max_levels[skill]) maxLevel = xp_tables.max_levels[skill];
 
     for (let i = 1; i <= maxLevel; i++) {
-        xp += xp_tables[table][i-1]
+        xp += xp_tables[table][i - 1];
 
         if (xp > experience) {
-            xp -= xp_tables[table][i-1]
+            xp -= xp_tables[table][i - 1];
         } else {
             if (i <= maxLevel) level = i;
         }
     }
 
-    let xpCurrent = Math.floor(experience - xp)
+    let xpCurrent = Math.floor(experience - xp);
 
     if (level < maxLevel) {
-        xpForNext = Math.ceil(xp_tables[table][level])
+        xpForNext = Math.ceil(xp_tables[table][level]);
     }
 
     progress = Math.max(0, Math.min(xpCurrent / xpForNext, 1));
@@ -49,5 +50,5 @@ module.exports = function calcSkill(skill, experience) {
         xpForNext,
         progress,
         levelWithProgress: level < maxLevel ? level + progress : level,
-    }
-}
+    };
+};
