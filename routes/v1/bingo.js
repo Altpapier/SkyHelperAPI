5ed738d588ccded6abb4f73be4a5e8172b313bff
DuckySoLucky <robertkovac160@gmail.com>
2022-09-01 19:54:37 +0200

    fix(bingo endpoint)

--- routes/v1/bingo.js
+++ routes/v1/bingo.js
@@ -15,6 +15,8 @@ module.exports = wrap(async function (req, res) {
     const profileRes = await makeRequest(res, `https://api.hypixel.net/skyblock/bingo?key=${process.env.HYPIXEL_API_KEY}&uuid=${uuid}`);
     const bingoRes = await makeRequest(res, `https://api.hypixel.net/resources/skyblock/bingo`);
 
+    if (bingoRes.data.id !== profileRes.data.events[profileRes.data.events.length-1].key) return res.status(200).json({status: 200, data: `Found no Bingo profiles for a user with a UUID of '${uuid}'` })
+    
     const profile = await parseBingoProfile(profileRes, bingoRes, uuid);
 
     return res.status(200).json({ status: 200, data: profile });
