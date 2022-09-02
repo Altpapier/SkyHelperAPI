a8814e0a12d078ef9c8c23631dda3ed516a33c95
DuckySoLucky <robertkovac160@gmail.com>
2022-09-02 14:05:01 +0200

    Revert UUID convertion

--- routes/v1/profiles.js
+++ routes/v1/profiles.js
@@ -6,9 +6,9 @@ const { parseHypixel, parseProfiles } = require('../../utils/hypixel');
 module.exports = wrap(async function (req, res) {
     let uuid = req.params.uuid;
     if (!isUuid(uuid)) {
-        const mojang_response = await makeRequest(res, `https://playerdb.co/api/player/minecraft/${uuid}`);
-        if (mojang_response?.data?.data?.player?.raw_id) {
-            uuid = mojang_response?.data?.data?.player?.raw_id;
+        const mojang_response = await makeRequest(res, `https://api.ashcon.app/mojang/v2/user/${uuid}`);
+        if (mojang_response?.data?.uuid) {
+            uuid = mojang_response.data.uuid.replace(/-/g, '');
         }
     }
 
