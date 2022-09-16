9576f61276063734f7aba650b8e43cab8863a485
Altpapier <62288830+Altpapier@users.noreply.github.com>
2022-09-16 18:09:36 +0200

    Delete example_response.js

--- example_response.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const axios = require('axios')
-
-setTimeout(async () => {
-    const response = (await axios.get('https://api.altpapier.dev/v1/fetchur?key=altpapier_test')).data
-    fs.writeFileSync("./examples/fetchur/example_response_full.json", JSON.stringify(response, null, 2));
-})
-
