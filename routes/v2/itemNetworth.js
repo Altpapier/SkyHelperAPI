8a63fbcf13fcaca626f4d79357cab4be7257fdc3
Altpapier <robin2leo@gmail.com>
2022-10-20 00:08:07 +0200

    fix(itemNetworth): matt forgot something smh + website docs

--- routes/v2/itemNetworth.js
+++ routes/v2/itemNetworth.js
@@ -6,7 +6,7 @@ module.exports = wrap(async function (req, res) {
     const data = req.body;
     if (!data) res.status(400).json({ status: 400, reason: 'Missing request body' });
 
-    const networth = await getItemNetworth(data, { prices: getPrices() });
+    const networth = await getItemNetworth(data.itemData, { prices: getPrices() });
 
     return res.status(200).json({ status: 200, data: networth });
 });
